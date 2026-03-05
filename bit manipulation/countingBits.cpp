e76a2c9381dae1fa018b591bcbc9b2c4d4266c2d
yhan <yhandesir01@gmail.com>
2026-03-05 16:18:35 +0800

    counting bits problem

---
+++ bit manipulation/countingBits.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+    public:
+        vector<int> countBits(int n) {
+            vector<int> result(n+1, 0);
+            for(int i=0; i<=n; i++) {
+                int cnt=0, n=i;
+                while(n) {
+                    cnt += n&1;
+                    n >>= 1;
+                }
+                result[i] = cnt;
+            }
+
+            return result;
+        }
+};
+
+// optimal solution with O(log n)
+class Solution2 {
+    public:
+        int countOnes(int k) {
+        int count = 0;
+        while (k) {
+            ++count;
+            k &= (k - 1);
+        }
+        return count;
+    }
+    vector<int> countBits(int n) {
+        vector<int> count(n + 1, 0);
+        for (int i = 1; i <= n; ++i)
+            count[i] = countOnes(i);
+        return count;
+    }
+};
