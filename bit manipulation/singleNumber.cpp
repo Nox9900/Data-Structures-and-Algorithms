3f5becaf558dbd7900c3b1fd499cede866bc5ef4
yhan <yhandesir01@gmail.com>
2026-03-05 16:21:42 +0800

    single number problem

--- bit manipulation/singleNumber.cpp
+++ bit manipulation/singleNumber.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 #include<vector>
+#include <unordered_map>
 using namespace std;
 
 class Solution {
     public:
-        int singeNumber() {}
+        vector<int> singleNumber(vector<int>& nums) {
+        unordered_map<int,int> freq;
+        for(int& c:nums) {
+            freq[c]++;
+        }
+        vector<int>res;
+        for(const auto& c:freq){
+            if(c.second==1) res.push_back(c.first);
+        }
+        return res;
+    }
 
 };
