9b7bd9a01aa15a316c318b369bec64e3c0ca8110
yhan <yhandesir01@gmail.com>
2026-03-06 11:02:47 +0800

    spiral matrix problem

--- matrix/spiralMatrix.cpp
+++ matrix/spiralMatrix.cpp
@@ -12,16 +12,19 @@ public:
         int top = 0, bottom = m - 1, left = 0, right = n - 1;
         
         while (top <= bottom && left <= right) {
+            // 1. Move right across top row
             for (int i = left; i <= right; i++) {
                 result.push_back(matrix[top][i]);
             }
             top++;
 
+            // 2. Move down along right column
             for (int i = top; i <= bottom; i++) {
                 result.push_back(matrix[i][right]);
             }
             right--;
             
+                // 3. Move left across bottom row (if still valid)
             if (top <= bottom) {
                 for (int i = right; i >= left; i--) {
                     result.push_back(matrix[bottom][i]);
@@ -29,6 +32,7 @@ public:
                 bottom--;
             }
             
+            // 4. Move up along left column (if still valid)
             if (left <= right) {
                 for (int i = bottom; i >= top; i--) {
                     result.push_back(matrix[i][left]);
