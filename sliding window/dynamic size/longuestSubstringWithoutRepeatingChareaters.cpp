992f42652b21a013f771c378463d2e3f4809360f
yhan <yhandesir01@gmail.com>
2026-03-06 11:57:56 +0800

    longuest substring without repeating characters problem

---
+++ sliding window/dynamic size/longuestSubstringWithoutRepeatingChareaters.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<int, int> res;
+        int l=0, cnt=0;
+        
+        // If character already seen and in current window
+        for(int r=0; r<s.size(); r++) {
+            if(res.find(s[r]) != res.end()) //Move left pointer past duplicate
+                l = max(res[s[r]]+1, l);
+
+            res[s[r]] = r; // Update/insert character's latest index
+            cnt = max(cnt, r-l+1); // Update max length
+        }
+        return cnt;
+    }
+};
