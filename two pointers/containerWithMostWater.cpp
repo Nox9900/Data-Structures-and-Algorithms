9342bddd987dad117f39bd5109e7f538967a7126
yhan <yhandesir01@gmail.com>
2026-03-07 11:36:44 +0800

    container with the most water problem

---
+++ two pointers/containerWithMostWater.cpp
@@ -0,0 +1,18 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int  left=0 ,right=height.size() - 1;
+        int maxArea = 0;
+        while(left<right) {
+            int minArea = min(height[left], height[right]) * (right - left);
+            maxArea = max(maxArea, minArea);
+            if(height[left]<height[right]) left++;
+            else right--;
+            
+        }
+        return maxArea;
+    }
+};
