c44b34060052558dc12f739f758d52c955b13d5e
yhan <yhandesir01@gmail.com>
2026-03-07 12:09:38 +0800

    product of array except self problem

---
+++ arrays/productOfArrayExceptSelf.cpp
@@ -0,0 +1,19 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n, 1);
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        int cur = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            res[i] = res[i] * cur * nums[i + 1];
+            cur = cur * nums[i + 1];
+        }
+        return res;
+    }
+};
