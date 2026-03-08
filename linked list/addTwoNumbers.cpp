f613c8c07c45c275c8f80209df8cef4aa77ccba6
yhan <yhandesir01@gmail.com>
2026-03-08 22:06:56 +0800

    add two numbers problem

---
+++ linked list/addTwoNumbers.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dump = new ListNode(0);
+        ListNode* p = l1;
+        ListNode* q = l2;
+        ListNode* curr = dump;
+        int carry = 0;
+
+        while(p || q){
+            int x = (p != nullptr) ? p->val : 0;
+            int y = (q != nullptr) ? q->val : 0;
+            int sum = x+y+carry;
+            carry = sum/10;
+
+            curr->next = new ListNode(sum%10);
+            curr = curr->next;
+
+            if(p) p = p->next;
+            if(q) q = q->next;
+        }
+
+        if(carry > 0) {
+            curr->next = new ListNode(carry);
+        }
+
+        ListNode* result = dump->next;
+        delete dump;
+        return result;
+
+    }
+};
