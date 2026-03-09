e3ecdfa612b64783e4ac0074f31e689c036059d5
yhan <yhandesir01@gmail.com>
2026-03-09 12:51:21 +0800

    design a min stack problem

---
+++ stack and queue/stack/minStack.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+class MinStack {
+private:
+    vector<int> st;
+    int minVal;
+public:
+    MinStack() {}
+    
+    void push(int val) {
+        if(st.empty()) {
+            minVal = val;
+        } else if(val <= minVal) {
+            st.push_back(minVal);
+            minVal = val;
+        }
+        st.push_back(val);
+    }
+    
+    void pop() {
+        int top = st.back();
+        st.pop_back();
+        if(top == minVal && !st.empty()) {
+            minVal = st.back();
+            st.pop_back();
+        }
+    }
+    
+    int top() {
+        return st.back();
+    }
+    
+    int getMin() {
+        return minVal;
+    }
+};
