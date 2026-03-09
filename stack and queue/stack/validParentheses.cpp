3a6a17fcf747e0784612814d6208d288e9674ca5
yhan <yhandesir01@gmail.com>
2026-03-09 12:47:13 +0800

    valid parenthese problem

---
+++ stack and queue/stack/validParentheses.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> res;
+        unordered_map<char, char>check = {
+            {'}', '{'},
+            {']', '['},
+            {')', '('}
+        };
+
+        for(char& c:s) {
+            if(check.count(c))
+                if(!res.empty() && res.top() == check[c]) res.pop();
+                else return false;
+            else res.push(c);
+        }
+        return res.empty();
+    }
+};
