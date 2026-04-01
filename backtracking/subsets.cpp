9c77f1cb796174fd3e2beaf6525ca8673842b941
yhan <yhandesir01@gmail.com>
2026-04-01 09:01:09 +0800

    subsets problem

---
+++ backtracking/subsets.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  vector<vector<int>>ans;
+private:
+    void sub(int ind,vector<int>& cur,vector<int>& nums){
+        if(ind==nums.size()){
+            ans.push_back(cur);
+            return;
+        }
+        cur.push_back(nums[ind]);
+        sub(ind+1,cur,nums);
+        cur.pop_back();
+        sub(ind+1,cur,nums);
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> cur;
+        sub(0,cur,nums);
+        return ans;
+    }
+};
