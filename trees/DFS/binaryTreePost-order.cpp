1ab9136b18d16d03165d8fc37e89290bcae770d6
yhan <yhandesir01@gmail.com>
2026-04-01 09:23:05 +0800

    binary tree postorder traversal problem

---
+++ trees/DFS/binaryTreePost-order.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+
+    void postOrder(TreeNode* p, vector<int>& res) {
+        if(!p) return;
+        postOrder(p->left, res);
+        postOrder(p->right, res);
+        res.push_back(p->val);
+
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if(!root) return res;
+        postOrder(root, res);
+        return res;
+    }
+};
