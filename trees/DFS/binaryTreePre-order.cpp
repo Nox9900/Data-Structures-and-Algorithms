c24f4197e14a71ec519fa1cf25c97892b04ecde4
yhan <yhandesir01@gmail.com>
2026-04-01 09:12:22 +0800

    binary tree preorder traversal problem

---
+++ trees/DFS/binaryTreePre-order.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+ 
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        if(!root) return result;
+
+        stack<TreeNode*> res;
+        res.push(root);
+
+        while(!res.empty()) {
+            TreeNode* node = res.top();
+            res.pop();
+            result.push_back(node->val);
+
+            if(node->right) res.push(node->right);
+            if(node->left) res.push(node->left);
+        }
+        return result;
+    }
+};
+
+
+class Solution2 {
+    public:
+        vector<int> preorder(TreeNode* root) {
+            vector<int> result;
+            preorder(root, result);
+            return result;
+        }
+
+        void preorder(TreeNode* root, vector<int> result) {
+            if(root=NULL) return;
+            result.push_back(root->val);
+            preorder(root->left, result);
+            preorder(root->right, result);
+        }
+
+
+};
