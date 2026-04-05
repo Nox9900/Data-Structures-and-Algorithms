467786ac437f73ffe1f65bc4099934c1a46bfca6
yhan <yhandesir01@gmail.com>
2026-04-05 11:56:30 +0800

    MyCalendar problem

---
+++ trees/myCalendar.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class MyCalendar {
+    map<int, int> calendar;
+public:
+    MyCalendar() {}
+    
+    bool book(int startTime, int endTime) {
+        auto prev = calendar.upper_bound(startTime);
+        if(prev != calendar.begin()) {
+            prev--;
+            if(prev->second > startTime ) return false;
+        }
+
+        auto next = calendar.lower_bound(startTime);
+        if(next != calendar.end() && next->first < endTime) {
+            return false;
+        }
+
+        calendar[startTime] = endTime;
+        return true;
+    }
+};
