9ff990f9f11253fa0a5e6a49180ce793e67c68ed
yhan <yhandesir01@gmail.com>
2026-04-05 12:03:33 +0800

    top k Elements problem

---
+++ top-k-Elements/top-K-Elements.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> cnt;
+        for(int& num:nums) cnt[num]++;
+
+        vector<vector<int>> bucket(nums.size()+1);
+        for(const auto& entry:cnt) {
+            int num = entry.first;
+            int freq = entry.second;
+            bucket[freq].push_back(num);
+        }
+
+        vector<int> top;
+        for(int i=bucket.size()-1; i >=0 && top.size()<k; i--) {
+            if(!bucket[i].empty()){
+                for(int& num : bucket[i]) {
+                    top.push_back(num);
+                    if(top.size() == k) return top;
+                }
+            }
+        }
+        return top;
+    }
+};
