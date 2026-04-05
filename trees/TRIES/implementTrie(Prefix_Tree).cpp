e2a74a6dacd2a67ecfa893571d22747e0762ee59
yhan <yhandesir01@gmail.com>
2026-04-05 11:58:33 +0800

    implement Trie (prefic Tree) problem

---
+++ trees/TRIES/implementTrie(Prefix_Tree).cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class TrieNode {
+    public:
+        unordered_map<char, TrieNode*> children; 
+        bool endOfWord ;
+        TrieNode() {
+            endOfWord = false;
+        } 
+};
+
+class Trie {
+private:
+    TrieNode* root;
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+    
+    void insert(string word) {
+        TrieNode* curr = root;
+        for(char ch:word) {
+            if(curr->children.find(ch) == curr->children.end())
+                curr->children[ch] = new TrieNode;
+            curr = curr->children[ch];
+        }
+        curr->endOfWord = true;
+    }
+    
+    bool search(string word) {
+        TrieNode* curr = root;
+        for(char ch:word) {
+            if(curr->children.find(ch) == curr->children.end())
+                return false;
+            curr = curr->children[ch];
+        }
+        return curr->endOfWord;
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode* curr = root;
+        for(char ch:prefix){
+            if(curr->children.find(ch) == curr->children.end()) 
+                return false;
+            curr = curr->children[ch];
+        }
+        return true;
+    }
+};
