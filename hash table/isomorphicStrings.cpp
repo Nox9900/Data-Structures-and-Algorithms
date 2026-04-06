18acfeb25eed0cef865c7a74cad93263ce121213
yhan <yhandesir01@gmail.com>
2026-04-06 13:36:37 +0800

    isomorphic strings problem

---
+++ hash table/isomorphicStrings.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.size() != t.size()) return false;
+        char mappedS[256] = {0};
+        char mappedT[256] = {0};
+    
+        for(int i=0; i<s.size(); i++) {
+            char ms = s[i];
+            char ts = t[i];
+            
+            if(mappedS[ms] ==0 && mappedT[ts] ==0) {
+                mappedT[ts] = ms;
+                mappedS[ms] = ts;
+            } else if(mappedT[ts] != ms || mappedS[ms] != ts) return false;
+        }
+        return true;
+    }
+};
