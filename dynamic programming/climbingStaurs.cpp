f0148fc46472abf9b8162fb5947e366ce5a25bbd
yhan <yhandesir01@gmail.com>
2026-04-20 13:24:00 +0800

    climbing stairs problem

---
+++ dynamic programming/climbingStaurs.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=1) return n;
+        vector<int> dp(n+1);
+        dp[0] = 0;
+        dp[1] = 0;
+        for(int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+};
