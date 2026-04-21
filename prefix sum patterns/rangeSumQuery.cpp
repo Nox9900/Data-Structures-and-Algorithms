aa4d5beefe735f171fd74f4b85337d4f8734fb40
yhan <yhandesir01@gmail.com>
2026-04-21 08:24:09 +0800

    prefix sum range query problem

---
+++ prefix sum patterns/rangeSumQuery.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class NumArray {
+private:
+    vector<int> prefix;
+public:
+    NumArray(vector<int>& nums) {
+        prefix.resize(nums.size());
+        prefix[0] = nums[0];
+        for(int i=0; i<nums.size(); i++) {
+            prefix[i] = prefix[i-1] + nums[i];
+        }
+    }
+    int rangeQuery(int left, int right) {
+        if(left==0) return prefix[right];
+        return prefix[right] - prefix[left-1];
+    }
+
+};
