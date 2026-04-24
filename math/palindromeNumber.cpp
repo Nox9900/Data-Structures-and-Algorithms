c8ebc9adf807cd987203e44e8b6d1f7b5f82212c
yhan <yhandesir01@gmail.com>
2026-04-24 11:43:42 +0800

    palindrome number problem

--- math/palindromeNumber.cpp
+++ math/palindromeNumber.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 class Solution {
-    int palindrome(int x) {
-        if(x<=0) return 0;
-        int s=0, o=x;
+    int IsPalindrome(int x) {
+        if(x<0) return 0;
+        long long s=0, o=x;
         while(o != 0) {
-            s = s*10 + 0%10;
+            s = s*10 + o%10;
             o /= 10;
         }
         return s==x;
