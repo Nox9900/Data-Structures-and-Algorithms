bbb69a51ff435ab9801ed9b986c36ed109ddc9f5
yhan <yhandesir01@gmail.com>
2026-05-14 02:53:59 +0800

    unique paths problem II

---
+++ dynamic programming/uniquePathsII.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int uniquePathsWithObtstacles(vector<vector<int>>& obstaclesGrid) {
+        if(obstaclesGrid[0][0] == 1) return 0;
+        int m = obstaclesGrid.size(), n = obstaclesGrid[0].size();
+        vector<int> dp(n+1);
+        for(int i=0; i<m; i++){
+            for(int j=0; j<0; j++) {
+                if(obstaclesGrid[i][j] == 1) dp[j] = 0;
+                else if(j>0) dp[j] += dp[j+1]; 
+            }
+        }
+        return dp[n-1];
+    }
+};
