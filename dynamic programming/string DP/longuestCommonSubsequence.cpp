4bd5f2bfe82e9499dc8869fe04d7db26287e72a6
yhan <yhandesir01@gmail.com>
2026-07-22 15:30:54 +0800

    longuest common subsequence problem

---
+++ dynamic programming/string DP/longuestCommonSubsequence.cpp
@@ -0,0 +1,30 @@
+#include<vector>
+
+/*
+    LONGUEST COMMON SUBSEQUENCE
+    https://leetcode.com/problems/longest-common-subsequence/submissions/
+
+*/
+
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+       int m = text1.size(), n = text2.size();
+       vector<int> prev(n+1, 0), curr(n+1, 0);
+
+       for(int i=1; i<=m; i++) {
+            for(int j = 1; j<=n ; j++) {
+                if(text1[i-1] == text2[j-1])
+                    curr[j] = 1 + prev[j-1];
+                else
+                    curr[j] = max(prev[j], curr[j-1]);
+            }
+            swap(prev, curr);
+            fill(curr.begin(), curr.end(), 0);
+       }
+
+       return prev[n];
+    }
+};
+
