de2cc8a8f3d737d6b9e74be77d09d1d735bd9686
yhan <yhandesir01@gmail.com>
2026-07-22 15:36:03 +0800

    unique path II problem

--- dynamic programming/2D Grid DP/uniquePathsII.cpp
+++ dynamic programming/2D Grid DP/uniquePathsII.cpp
@@ -7,6 +7,8 @@ public:
         if(obstaclesGrid[0][0] == 1) return 0;
         int m = obstaclesGrid.size(), n = obstaclesGrid[0].size();
         vector<int> dp(n+1);
+        dp[0] = 1;
+
         for(int i=0; i<m; i++){
             for(int j=0; j<0; j++) {
                 if(obstaclesGrid[i][j] == 1) dp[j] = 0;
