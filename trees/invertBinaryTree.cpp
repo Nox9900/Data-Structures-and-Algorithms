b201295a5f29faf48916bba3ac34bfc7890d0b00
yhan <yhandesir01@gmail.com>
2026-07-22 16:20:02 +0800

    invert binary tree problem

---
+++ trees/invertBinaryTree.cpp
@@ -0,0 +1,26 @@
+
+/*
+    INVERT BINARY TREE
+    https://leetcode.com/problems/invert-binary-tree/
+*/
+
+* struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root) return nullptr;
+        swap(root->left, root->right);
+        invertTree(root->left);
+        invertTree(root->right);
+
+        return root;
+    }
+};
